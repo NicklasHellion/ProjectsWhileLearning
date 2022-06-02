5246ad9af523c977f6d6fd9f9cbd136f19bd6922
NicklasHellion <nicola.raduazzo@gmail.com>
2022-06-02 15:37:48 +0200

    blackjack game exercise final

--- blackjack game/script.js
+++ blackjack game/script.js
@@ -1,20 +1,37 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let player = {
+  name: "Nicola",
+  chips: 145,
+};
+
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": € " + player.chips;
+
 function getRandomCard() {
-  let floor = Math.floor(Math.random() * 11) + 1;
-  return floor;
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber === 1) {
+    return 11 || 1;
+  } else if (randomNumber > 10) {
+    return 10;
+  } else {
+    return randomNumber;
+  }
 }
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
@@ -38,17 +55,10 @@ function renderGame() {
 }
 
 function newCard() {
-  console.log("Draw a new card from the deck");
-  let card = getRandomCard();
-  sum += card;
-  cards.push(card);
-  console.log(cards);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
-
-// function rollDice() {
-//   let floor = Math.floor(Math.random() * 6) + 1;
-//   return floor;
-// }
-
-// console.log(rollDice());
