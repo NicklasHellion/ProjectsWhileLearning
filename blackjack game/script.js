6f0eaecba11235f8f08cf6e9a3a909b0e98b4889
NicklasHellion <nicola.raduazzo@gmail.com>
2022-06-02 13:16:36 +0200

    arrays and random numbers added

--- blackjack game/script.js
+++ blackjack game/script.js
@@ -1,5 +1,6 @@
-let firstCard = 6;
-let secondCard = 9;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -8,11 +9,21 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+function getRandomCard() {
+  return 5;
+}
+
 function startGame() {
   renderGame();
 }
 
 function renderGame() {
+  //messageEl.textContent = message;
+  cardsEl.textContent = "Cards: ";
+  for (let x = 0; x < cards.length; x++) {
+    cardsEl.textContent += cards[x] + " ";
+  }
+  sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -22,14 +33,13 @@ function renderGame() {
     message = "You lost!";
     isAlive = false;
   }
-  messageEl.textContent = message;
-  sumEl.textContent = "Sum: " + sum;
-  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
 }
 
 function newCard() {
   console.log("Draw a new card from the deck");
-  let card = 1;
+  let card = getRandomCard();
   sum += card;
+  cards.push(card);
+  console.log(cards);
   renderGame();
 }
