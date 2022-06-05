306c568d82a876c942b656927d2ce730d8324122
NicklasHellion <nicola.raduazzo@gmail.com>
2022-06-06 01:50:46 +0200

    chrome API integrated, final version deployed

--- chrome extension/script.js
+++ chrome extension/script.js
@@ -3,6 +3,7 @@ let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const tabBtn = document.getElementById("tab-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
@@ -10,6 +11,14 @@ if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 function render(leads) {
   let listItems = "";
@@ -26,13 +35,11 @@ function render(leads) {
   ulEl.innerHTML = listItems;
 }
 
-deleteBtn.addEventListener("dblclick", function () {
+deleteBtn.addEventListener("click", function () {
   localStorage.clear();
   myLeads = [];
   render(myLeads);
 });
-
-console.log(myLeads);
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
